a92dc3d22a26cc78af32047046926a4443c1c360
apanichella <a.panichella@tudelft.nl>
2020-09-05 19:35:56 +0200

    Updating the fitness functions

--- fitness_functions.R
+++ fitness_functions.R
@@ -1,74 +1,117 @@
+library(philentropy)
+
+source(paste(main_path, "/lda_utilities.R", sep=""))
+
 # This function implements the Silhouette Coefficient (fitness function to maximize).  
 # The Silhouette coefficient takes values in [-1; 1]. Higher Silhouette Coefficient  
 # denotes better clustering quality
-fitness_LDA<-function(x=c()){
-  numero_topic<-round(x[1]) #x[1] = number of topics k
-  iteration<-round(x[2])    #x[2] =  number of gibbs iteration
-  pAlpha<-x[3]              #x[3] = Alpha
-  pDelta<-x[4]              #x[4] = Beta
-  
-  # apply LDA to the term-by-document matrix
-  ldm <- LDA(tdm, method="Gibbs", control = list(alpha=pAlpha, delta=pDelta, iter=iteration, seed=5, nstart=1), k = numero_topic)  # k = num of topics
-  pldm <- posterior(ldm)
-  names(pldm)
-  
-  # compute the topic-by-term matrix    
-  names(tdm$dimnames)
-  docs<-tdm$dimnames$Docs
-  topics<-names(terms(ldm))
-  matrix<-pldm$topics
-  dimnames(matrix)<-list(docs,topics)
-  
-  # compute the distance between documents in the topics space
-  distances <- as.matrix(dist(matrix, method = "euclidean", diag = T, upper = T))
-  
-  # computing number of clusters
-  clustering<-matrix("",length(rownames(matrix)),1)
-  for (i in 1:length(rownames(matrix))) {
-    flag<-(matrix[i,]==max(matrix[i,]))# each documents belongs to the cluster with the higher probability
-    flag<-which(flag==TRUE)
-    topics <- sort(flag)
-    clustering[i,1]<-paste(topics, collapse = '_')
-  }
-  rownames(clustering)<-rownames(matrix)
-  
-  # assign the clusters
-  clusters<-unique(clustering)
-  count <- 1
-  for (clust in clusters){
-    clustering[clustering[,1] == clust,1] <- count
-    count <- count+1
-  }
-   cluster_objects<-list(); 
-   cluster_objects$clustering <- as.numeric(clustering)
-  
-  # compute the cohesion for each documents
-  cohesion <- matrix(nrow = length(rownames(distances)), ncol = 1)
-  for (i in 1:length(rownames(distances))){
-    cohesion[i,1] <- max(distances[clustering[,1] == clustering[i,1],i])
+fitness_max <- function(x=c()){
+  n = length(x)
+  for (index in 1:n){
+    if (x[index] < lower_bounds[index] || x[index] > upper_bounds[index])
+      return(-Inf)
   }
   
-  # compute the separation from other clusters 
-  separation <- matrix(nrow = length(rownames(distances)), ncol = 1)
-  for (i in 1:length(rownames(distances))){
-    separation[i,1] <- min(distances[clustering[,1] != clustering[i,1],i])
+  if (fitness_name  == "silhouette"){
+    lda_model = train_lda(x, lib, tdm)
+    document2topic = document_by_topic_mixture(lda_model, lib)
+    
+    # compute the distance between documents in the topics space
+    distances <- as.matrix(dist(document2topic, method = "euclidean", diag = T, upper = T))
+    coefficient <- silhouette_coefficient(document2topic, distances)
+    return(coefficient)
+  } else if (fitness_name  == "coherence"){
+    lda_model = train_lda(x, lib, tdm)
+    top_words = get_top_words(lda_model, lib, 10)
+    numero_topic = round(x[1])
+    coherence <- matrix(nrow = numero_topic, ncol = 1)
+    for (i in 1:numero_topic){
+      coherence[i,1] = SpeedReader::topic_coherence(top_words[,i], sparse_tdm, vocabulary = colnames(sparse_tdm), numeric_top_words = FALSE, K=10)
+    }
+    return(mean(coherence, na.rm=T))
+  } else if (fitness_name == "raw_score") {
+    n_topics = x[1]
+    n_runs = 4
+    n = 7 # number of overlap words
+    runs  <- vector(mode = "list", length = n_runs)
+    count <- 1
+    x2 <- x
+    for (i in 1:n_runs){
+      tdm <- shuffle_tdm(tdm)
+      for (j in 1:n_runs){
+        lda_model = train_lda(x2, lib, tdm)
+        top_words = get_top_words(lda_model, lib, 10)
+        runs[[count]] <- list(top_words)
+        count = count + 1
+        remove(lda_model)
+        }
+      .jcall("java/lang/System", "V", "gc") # call the garbage collector
+      gc()
+      }
+    scores <- matrix(data=0, nrow = 1, ncol = n_topics)
+    for (topic_index in 1:n_topics){
+      run1 <- runs[[1]][[1]]
+      for (run_index in 2:(n_runs*2)){
+        run2 <- runs[[run_index]][[1]]
+        count <- overlap_count(run1[,topic_index], run2)
+        if (max(count) >= n){
+          scores[topic_index] = scores[topic_index]+1
+        }
+      }
+    }
+    median(scores/n_runs)
+    return(median(scores/n_runs))
+  } else {
+    stop(paste("Fitness function", fitness_name, "not found!"), call. = TRUE)
   }
+}
+
+overlap_count <- function(topic, topic_matrix){
+  n_topics <- ncol(topic_matrix)
+  count <- matrix(0, 1, n_topics)
+  for (index in 1:n_topics){
+    intersection = intersect(topic, topic_matrix[,index])
+    count[index] = length(intersection)
+  }
+  return(count)
+}
+
+test_fitness <- function(){
+  runs <-  vector(mode = "list", length = 4)
+  for (i in 1:4)
+    runs[[i]] <-  list(matrix("",10,4))
   
-  # compute the silhouette coefficient
-  sil <- matrix(nrow = length(rownames(distances)), ncol = 1)
-  for (i in 1:length(rownames(distances))){
-    if (sum(clustering[i,1] == clustering)>1)
-      sil[i,1] <- (separation[i,1] - cohesion[i,1]) / max(separation[i,1], cohesion[i,1])
-    else
-      sil[i,1] <- 0 # if the cluster contanis only one document, the Silohuette Coeff. is zero
-  }
-  return(mean(sil))
+  runs[[1]][[1]][,1]<-c("glori", "telemetri", "command", "spacecraft", "trace", "smrd", "tim", "spec", "pip", "parent")
+  runs[[1]][[1]][,2]<-c("spec", "smrd", "parent", "child", "glori", "artifact", "referenc", "verif", "matrix", "data")
+  runs[[1]][[1]][,3]<-c("test", "case", "accuraci", "roll", "document", "glori", "plan", "pitch", "yaw", "valu")
+  runs[[1]][[1]][,4]<-c("command", "specifi", "ground", "softwar", "telemetri", "initi", "pip", "data", "configur", "band")
+  
+  runs[[2]][[1]][,1]<-c("command", "specifi", "ground", "softwar", "initi", "telemetri", "data", "pip", "configur", "band")
+  runs[[2]][[1]][,2]<-c("document", "test", "glori", "initi", "plan", "roll", "case", "pitch", "yaw", "point")
+  runs[[2]][[1]][,3]<-c("spec", "smrd", "parent", "child", "glori", "artifact", "verif", "points", "matrix", "spacecraft")
+  runs[[2]][[1]][,4]<-c("pip", "capabi", "glori", "ground", "command", "smrd", "spec", "tim", "list", "spacecraft")
+  
+  runs[[3]][[1]][,1]<-c("spec", "smrd", "parent", "child", "glori", "referenc", "artifact", "verif", "matrix", "spacecraft")
+  runs[[3]][[1]][,2]<-c("command", "telemetri", "pip", "ground", "spacecraft", "mode", "software", "document", "spec", "glori")
+  runs[[3]][[1]][,3]<-c("command", "specifi", "ground", "softwar", "initi", "telemetri", "data", "configur", "pip", "band")
+  runs[[3]][[1]][,4]<-c("test", "initi", "accuraci", "glori", "roll", "plan", "matrix", "yaw", "pitch", "valu")
+  
+  runs[[4]][[1]][,1]<-c("command", "specifi", "tim", "pip", "ground", "telemetri", "glori", "includ", "softwar", "document")
+  runs[[4]][[1]][,2]<-c("test", "initi", "glori", "plan", "roll", "accuraci", "case", "pip", "point", "yaw")
+  runs[[4]][[1]][,3]<-c("command", "specifi", "ground", "softwar", "telemetri", "initi", "data", "pip", "configur", "band")
+  runs[[4]][[1]][,4]<-c("spec", "smrd", "child", "glori", "artifact", "referenc", "parent", "verif", "matrix", "data")
+}
+
+jaccard_similarity <- function(v1, v2){
+  U = union(v1, v2)
+  I = intersect(v1, v2)
+  return(length(I)/length(U))
 }
 
 # This function is a wrapper around the silhouette coefficient. Minimizing this function corresponds 
 # to maximizing the original silhouette coefficient. Meta-heuristics that minimize the fitness 
 # function should use this function.
-min_fitness_LDA<-function(x=c()){
-  value <- 2-fitness_LDA(x)
+fitness_min <- function(x=c()){
+  value <- (-fitness_max(x))
   return(value)
 }
